20acda78f5402793c5d77fb664ba829f492815dd
Tom Hazell <hazell.tom12@gmail.com>
2017-01-08 23:31:44 +0000

    added /tweets.json for easy acces to data

--- src/main/java/com/tomhazell/twitter/console/tweets/TweetsController.java
+++ src/main/java/com/tomhazell/twitter/console/tweets/TweetsController.java
@@ -1,5 +1,6 @@
 package com.tomhazell.twitter.console.tweets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tomhazell.twitter.console.TwitterAction;
 import com.tomhazell.twitter.console.TwitterActionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,17 +21,31 @@ import java.util.List;
 public class TweetsController {
 
     public static final String ENDPOINT_TWEETS = "/tweets";
+    public static final String ENDPOINT_TWEETS_CSV = "/tweets.json";
 
     public static final String VIEW_TWEETS = "allTweets";
 
     public static final String MODEL_ATTR_TWEETS_LIST = "tweets";
+
     @Autowired
     TwitterActionRepository twitterActionRepository;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @RequestMapping(ENDPOINT_TWEETS)
-    public ModelAndView getTweets(Model model){
+    public ModelAndView getTweets(Model model) {
         List<TwitterAction> all = twitterActionRepository.findAll();
         model.addAttribute(MODEL_ATTR_TWEETS_LIST, all);
         return new ModelAndView(VIEW_TWEETS);
+    }
+
+    @RequestMapping(ENDPOINT_TWEETS_CSV)
+    public String csvTweets(Writer writer) throws IOException {
+        List<TwitterAction> all = twitterActionRepository.findAll();
+
+        writer.write(mapper.writeValueAsString(all));
+
+        return null;
     }
 }
